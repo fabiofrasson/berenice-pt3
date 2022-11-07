22fbffa4184e9fbbec31cb44b57efcde4604a775
JosueObadovski <101779621+JosueObadovski@users.noreply.github.com>
2022-11-06 22:37:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 #include <string.h>
 #include <locale.h>
 #include <time.h>
+#include <stdbool.h>
 
 void menu();
 void produtos();
-void vendas();
 void sair();
 void limparTela();
 void exibir();
@@ -17,20 +17,37 @@ int escolherItemMenu();
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    int opcaoMenu = escolherItemMenu();
     menu();
+}
 
-    if(opcaoMenu == 1){ // Exibir submenu de Produtos
+void menu()
+{
+    printf("---------------------------------------------------------------------------------------------\n");
+    printf("\t\t\tBem-Vindo ao Mercado da Berenice\n\n");
+    printf("\t\t\tSelecione um dos números referentes a operação\n");
+    printf("\t\t\t1- Produtos\n");
+    printf("\t\t\t2- Vendas\n");
+    printf("\t\t\t3- Sair\n");
+    printf("---------------------------------------------------------------------------------------------\n");
+
+    int opcaoMenu = escolherItemMenu();
+
+    switch(opcaoMenu)
+    {
+    case 1:
+        limparTela();
         produtos();
+        break;
+    case 2:
+        limparTela();
+        submenuvendas();
+        break;
+    case 3:
+        limparTela();
+        sair();
+        break;
     }
-        else if(opcaoMenu == 2){ // Exibir Submenu de Vendas
-                vendas();
-        }
-            else if(opcaoMenu == 3){ // Sair do programa
-                sair();
-            }
-                    return 0;
-                }
+}
 
 int escolherItemMenu()
 {
@@ -51,22 +68,13 @@ int escolherItemMenu()
     return resposta;
 }
 
-void menu(){
-    printf("---------------------------------------------------------------------------------------------\n");
-    printf("\t\t\tBem-Vindo ao Mercado da Berenice\n\n");
-    printf("\t\t\tSelecione um dos números referentes a operação\n");
-    printf("\t\t\t1- Produtos\n");
-    printf("\t\t\t2- Vendas\n");
-    printf("\t\t\t3- Sair\n");
-    printf("---------------------------------------------------------------------------------------------\n");
-}
-
-void produtos(){
+void produtos()
+{
     int opcaoprodutos;
 
     printf("---------------------------------------------------------------------------------------------\n");
     printf("\t\tProdutos\n\n");
-    printf("\t\tSelecione um dos números referentes a operação");
+    printf("\t\tSelecione um dos números referentes a operação\n");
     printf("\t\t1- Exibir\n");
     printf("\t\t2- Cadastrar\n");
     printf("\t\t3- Atualizar\n");
@@ -78,37 +86,64 @@ void produtos(){
     scanf("%d", &opcaoprodutos);
     getchar();
 
-    while(opcaoprodutos != 1 && opcaoprodutos != 2 && opcaoprodutos != 3&& opcaoprodutos != 4 && opcaoprodutos != 5 && opcaoprodutos != 6 && opcaoprodutos != 7){
+    while(opcaoprodutos != 1 && opcaoprodutos != 2 && opcaoprodutos != 3&& opcaoprodutos != 4 && opcaoprodutos != 5 && opcaoprodutos != 6 && opcaoprodutos != 7)
+    {
 
-    limparTela();
+        limparTela();
 
-    produtos();
+        produtos();
 
-    printf("Erro: Opção inválida. Por favor, digite uma opção válida:\n");
-    scanf("%d", &opcaoprodutos);
-    getchar();
-
-    if(opcaoprodutos = 7){
+        printf("Erro: Opção inválida. Por favor, digite uma opção válida:\n");
+        scanf("%d", &opcaoprodutos);
+        getchar();
+    }
+    switch(opcaoprodutos)
+    {
+    case 1:
+        limparTela();
+        exibir();
+        limparTela();
+        produtos();
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    case 7:
+        limparTela();
         menu();
+        break;
     }
 }
 
-void exibir(){
-
+void exibir()
+{
+    int voltar;
     int codigo[5] = {18475, 45462, 34654, 40, 5000};
     float preco[5] = {7.50, 8.69, 5.00, 4.50, 3.25};
 
     printf("---------------------------------------------------------------------------------------------\n");
-    printf("\t\tItem(Código)\tNome do Item\tValor(Unidade)\tEstoque");
-    printf("\t\t\t\t%i\t\t\t%s\t\tR$%.2f\t\t\t%d\n", codigo[0], "Pão de Forma", preco[0]); //estoqueItens[0]);
-    printf("\t\t\t\t%i\t\t\t%s\t\tR$%.2f\t\t\t%d\n", codigo[1], "Pão de Centeio", preco[1]); //estoqueItens[1]);
-    printf("\t\t\t\t%i\t\t\t%s\t\tR$%.2f\t\t\t%d\n", codigo[2], "Broa de Milho", preco[2]); //estoqueItens[2]);
-    printf("\t\t\t\t%i\t\t\t%s\t\t\tR$%.2f\t\t\t%d\n", codigo[3], "Sonho", preco[3]); //estoqueItens[3]);
-    printf("\t\t\t\t%i\t\t\t%s\t\t\tR$%.2f\t\t\t%d\n", codigo[4], "Tubaína", preco[4]); //estoqueItens[4]);
+    printf("\t\tItem(Código)\tNome do Item\tValor(Unidade)\tEstoque\n");
+    printf("\t\t%i\t\t%s\tR$%.2f\t\t%d\n", codigo[0], "Pão de Forma", preco[0]); //estoqueItens[0]);
+    printf("\t\t%i\t\t%s\tR$%.2f\t\t%d\n", codigo[1], "Pão de Centeio", preco[1]); //estoqueItens[1]);
+    printf("\t\t\%i\t\t%s\tR$%.2f\t\t%d\n", codigo[2], "Broa de Milho", preco[2]); //estoqueItens[2]);
+    printf("\t\t%i\t\t%s\t\tR$%.2f\t\t%d\n", codigo[3], "Sonho", preco[3]); //estoqueItens[3]);
+    printf("\t\t%i\t\t%s\t\tR$%.2f\t\t%d\n", codigo[4], "Tubaína", preco[4]); //estoqueItens[4]);
     printf("---------------------------------------------------------------------------------------------\n");
+    printf("Digite um número qualquer para voltar para o menu\n");
+    scanf("%d", &voltar);
+    getchar();
 }
 
-void submenuvendas(){
+void submenuvendas()
+{
+    limparTela();
     int opcaovendas;
     printf("\t\t\tSelecione um dos números referentes a operação\n");
     printf("\t\t\t1- Realizar venda\n");
@@ -118,35 +153,42 @@ void submenuvendas(){
     scanf("%d", &opcaovendas);
     getchar();
 
-    while(opcaovendas != 1 && opcaovendas != 2 && opcaovendas != 3){
+    while(opcaovendas != 1 && opcaovendas != 2 && opcaovendas != 3)
+    {
 
-    limparTela();
+        limparTela();
 
-    submenuvendas();
+        submenuvendas();
 
-    printf("Erro: Opção inválida. Por favor, digite uma opção válida:\n");
-    scanf("%d", &opcaovendas);
-    getchar();
+        printf("Erro: Opção inválida. Por favor, digite uma opção válida:\n");
+        scanf("%d", &opcaovendas);
+        getchar();
+    }
 
-    if(opcaovendas = 3){
+    switch(opcaovendas)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        limparTela();
         menu();
     }
 }
 
-}
-
-void sair(){
+void sair()
+{
     return 0;
 }
 
-void limparTela(){
-
-#ifdef __linux__
+void limparTela()
+{
+#ifdef linux
     system.("clear");
 #elif _WIN32
     system("cls");
 #else
 
 #endif
-
 }
